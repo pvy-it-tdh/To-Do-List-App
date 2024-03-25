8d68b7ca2ff0bca127eb38932c210701af1fc43e
pvy-it-tdh <phucvy107@gmail.com>
2024-03-25 20:40:16 +0700

    add module addTask

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function app() {
+const inputbox=document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
+function addTask()
+{
+    if(inputbox.value===''){
+        alert("You must write something!");
+    }else{
+        // tạo ra một thẻ li
+        let li=document.createElement("li");
+        // gán giá trị của thẻ li bằng giá trị của inputbox
+        li.innerHTML=inputbox.value
+        // hiển thị thẻ li trong list-container
+        listContainer.appendChild(li);
+    }  
 }
